a6c692621ec3766cef76431d145bff4db1ba83b3
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-10-31 19:08:29 +0530

    ESLint configuraion

--- src/server/server.js
+++ src/server/server.js
@@ -4,3 +4,19 @@ console.log(`hello ${blegh}`);
 const obj = {hey: 1};
 const obj2 = {...obj, blegh: 2};
 console.log(obj2);
+
+class AppComponent {
+
+}
+
+switch (blegh) {
+	case 1:
+		console.log("HEt");
+		break;
+    
+	case 2:
+		console.log("WHOA");
+		break;
+}
+
+console.log(<AppComponent/>);
