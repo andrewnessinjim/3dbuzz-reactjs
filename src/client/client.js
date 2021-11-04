550fae1b482ece898ef530d76a29dc245c543f60
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-11-04 13:47:31 +0530

    Remove unused variable

--- src/client/client.js
+++ src/client/client.js
@@ -4,7 +4,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter as Router} from "react-router-dom";
 
-import * as A from "./actions";
 import { StoreProvider } from "./lib/component";
 import {Dispatcher} from "shared/dispatcher";
 import createStores from "./stores";
