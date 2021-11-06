564099a382de30b8c62e76c73abd9e16d73618e2
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-11-06 19:54:54 +0530

    Comments for observable dialog events

--- src/client/stores/app.ts
+++ src/client/stores/app.ts
@@ -6,12 +6,13 @@ export default class AppStore {
 		this.dialog$ = dispatcher
 			.on$(A.DIALOG_SET)
 			.scan((stack, action) => {
-				_.remove(stack, {id:action.id});
 
+				//If the dialog is already open, then remove it and bring it to the top of the stack
+				_.remove(stack, {id:action.id});
 				if(action.isOpen)
 					stack.push({id: action.id, props: action.props});
 
-				return stack;
+				return stack; //Stack represents the list of open dialogs, starts as an empty array
 			}, [])
 			.startWith([])
 			.publishReplay(1);
